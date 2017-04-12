58e0aaadd0d30e4c8257a74f4e4067314425601f
Lorenzo <sheepytaggernep@gmail.com>
2017-04-12 13:49:25 +0200

    Added project

---
+++ src/main/java/boundary/rest/aids.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boundary.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author 878550
+ */
+@Stateless
+@Path("")
+public class aids {
+
+    @GET
+    public String test(){
+        return "it works!";
+    }
+}
